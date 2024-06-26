ffc0d1fb3e2109a5dafa9eef4c581a4620d30f62
abiudy <nyagaka254@gmail.com>
2024-06-26 20:48:32 +0300

    Integer operations

--- Learn-Web-Development/JavaScript/Lesson 1 - Syntax Basics & Types/sandbox.js
+++ Learn-Web-Development/JavaScript/Lesson 1 - Syntax Basics & Types/sandbox.js
@@ -1,15 +1,35 @@
-//common string commands
+let radius = 10;
+const pi = 3.14;
 
-let email = "fury@high.co.ke";
+//console.log(radius, pi);
 
-//let result = email.lastIndexOf("h");
+// math operators +, -, *, /, **, %
+// console.log(10 / 2);
+// let result = radius % 3;
+//let result = pi * radius ** 2;
 
-/* let result = email.slice(3, 7); */
+// // order of operation - B I D M A S
+//
+// let result = 5 * (10 - 3) ** 2;
+//
+// console.log(result);
 
-//let result = email.substring(4, 10);
+let likes = 10;
 
-//let result = email.replace("u", "i");
+//likes = likes + 1;
+//likes++;
+//likes--;
+//likes += 10;
+//likes -= 5;
+//likes *= 2;
+// likes /= 2;
+//
+// console.log(likes);
 
-let result = email.replace("h", "w");
+// NaN - not a number
 
+// console.log(5 / "hello");
+// console.log(5 * "hello");
+
+let result = "the blog has " + likes + " likes";
 console.log(result);
