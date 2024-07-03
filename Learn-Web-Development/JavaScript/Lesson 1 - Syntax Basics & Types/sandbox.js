7f44424dd71343f17a27bb05a7b4b1a7946ae3ad
abiudy <nyagaka254@gmail.com>
2024-07-03 16:37:22 +0300

    booleans

--- Learn-Web-Development/JavaScript/Lesson 1 - Syntax Basics & Types/sandbox.js
+++ Learn-Web-Development/JavaScript/Lesson 1 - Syntax Basics & Types/sandbox.js
@@ -1,3 +1,30 @@
-let age = null;
+// booleans & comparisons
+console.log(true, false, "true", "false");
 
-console.log(age, age + 3, `the age is ${age}`);
+// methods can return booleans
+let email = "allan@net.com";
+let names = ["batman", "robin", "catwoman"];
+
+// let result = email.includes("!");
+// let result = names.includes("alfred");
+
+// console.log(result);
+
+// comparison operators
+let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 35);
+// console.log(age != 35);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 25);
+
+let name = "allan";
+
+console.log(name == "allan");
+console.log(name == "Allan");
+console.log(name > "nyagaka");
+console.log(name > "Allan");
+console.log(name > "Nyagaka");
