74537cc84ce6c8c7f295a424e6fbf7dec742aa8b
abiudy <nyagaka254@gmail.com>
2024-07-03 16:16:28 +0300

    null and undefined

--- Learn-Web-Development/JavaScript/Lesson 1 - Syntax Basics & Types/sandbox.js
+++ Learn-Web-Development/JavaScript/Lesson 1 - Syntax Basics & Types/sandbox.js
@@ -1,22 +1,3 @@
-let robins = ["Dick", "Tim", "Jason"];
+let age = null;
 
-// robins[1] = "Stephanie";
-// console.log(robins[1]);
-
-// let ages = [20, 25, 30, 35];
-// console.log(ages[2]);
-
-// let random = ["shaun", 20, 30];
-// console.log(random);
-
-// array properties
-//console.log(robins.length);
-
-// array methods
-//let result = robins.join("-");
-//let result = robins.indexOf("Tim");
-//let result = robins.concat(["Damian", "Stephanie Brown"]);
-let result = robins.push("Damian");
-result = robins.pop();
-
-console.log(result);
+console.log(age, age + 3, `the age is ${age}`);
