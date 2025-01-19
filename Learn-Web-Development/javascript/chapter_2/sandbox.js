5d6fd832c44a7529cd4ad3d868de119ac5b22393
abiudy <nyagaka254@gmail.com>
2025-01-19 17:26:11 +0300

    javascript changes

---
+++ Learn-Web-Development/javascript/chapter_2/sandbox.js
@@ -0,0 +1,25 @@
+//strings
+console.log("Test");
+let email = "abiudy@gmail.com";
+console.log(email);
+
+//string concatenation
+let fname = "Allan";
+let lname = "Abiud";
+
+let fullname = fname + " " + lname;
+console.log(fullname);
+
+// get characters
+console.log(fullname[3]);
+
+//string length
+console.log(fullname.length);
+
+//string methods
+console.log(fullname.toUpperCase());
+let result = fullname.toLowerCase();
+console.log(result, fullname);
+
+let index = email.indexOf("@");
+console.log(index);
